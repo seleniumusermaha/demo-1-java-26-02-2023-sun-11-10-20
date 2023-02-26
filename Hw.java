290f7c6a0a47d4ec3bb109c08f0bd3c4d694046c
unknown <kmahaganeshsample@outlook.com>
2023-02-26 10:54:22 +0530

    saved

---
+++ Hw.java
@@ -0,0 +1,53 @@
+package hw.java;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Hw {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();String c = sc.next();int b = sc.nextInt();
+		if (c.equals("+")) {
+			int e = a + b;
+			System.out.println("--");
+			System.out.println(e);
+			System.out.println("--");
+		}
+		if (c.equals("-")) {
+			int e = a - b;
+			System.out.println("--");
+			System.out.println(e);
+			System.out.println("--");
+		}
+		if (c.equals("*")) {
+			int e = a * b;
+			System.out.println("--");
+			System.out.println(e);
+			System.out.println("--");
+		}
+		if (c.equals("/")) {
+			int e = a / b;
+			System.out.println("--");
+			System.out.println(e);
+			System.out.println("--");
+
+		}
+		if (c.equals("%")) {
+			int e = a % b;
+			System.out.println("--");
+			System.out.println(e);
+			System.out.println("--");
+		}
+
+		/*
+		 * switch (c) { case "+": int d = a+b; break; case "-": int d = a-b; break; case
+		 * "*": int d = a+b; break;
+		 * 
+		 * default: break; }
+		 */
+
+	}
+}
